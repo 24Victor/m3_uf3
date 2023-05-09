34cb90eca0f71635d914dfe5c9ad323e50a17665
24Victor <borenvictorzheng@iesmontsia.org>
2023-05-09 14:14:34 +0200

    Co-authored-by: IkerGisbert <IkerGisbert@users.noreply.github.com> Co-authored-by: CRISTIAN ALEXANDRU CUTAR <CCutar@users.noreply.github.com>

---
+++ words.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class words{
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        int opcio = 0;
+        do {
+            System.out.println("============================");
+            System.out.println("Benvingut al programa WORDS!");
+            System.out.println("============================");
+            System.out.println("Prem 1 per a jugar en catala o 2 per jugar en angles");
+            System.out.println("Prem 3 per a mostrar el registre");
+            System.out.println("Prem 4 per a sortir del programa");
+            
+            int decisio = sc.nextInt();
+
+            switch(opcio){
+                case 1:
+                    
+                break;
+
+                case 2:
+
+                break;
+
+                case 3:
+                
+                break;
+            }
+        } while (opcio <= 4);
+    }
+
+    public static void wordgame (){
+        
+    }
+}
