c25bd02b3324e060eabee9500841ba5e032166df
24Victor <borenvictorzheng@iesmontsia.org>
2023-05-16 16:38:41 +0200

    fas

--- IOFile.java
+++ IOFile.java
@@ -5,18 +5,29 @@ class IOFile{
     public static void main (String[] args) {
 		IOFile programa = new IOFile();
         String paraules_partides = "paraules-partides.txt";
-		programa.llegirFitxer(paraules_partides);
+        Words[] w = new Words[10];
+		programa.llegirFitxer(paraules_partides, w);
 	}
     
-    public void llegirFitxer(String paraules_partides){
+    public void llegirFitxer(String paraules_partides, Words[] w){
         //Llegir lo que hi ha en el fixer i imprimir-ho.
         try{
             File file = new File(paraules_partides);
             Scanner lector = new Scanner(file);
+
+            int contador = 0;
 			//Si s'executa aquesta instrucció, s'ha obert el fitxer
-			while(lector.hasNextLine()) {
+			while(lector.hasNextLine() && contador < w.length) {
                 //GUARDAR LA LINEA EN UNA VARIABLE Y IMPRIMIRLA
-				System.out.println(lector.nextLine());
+                String linea = (lector.nextLine());
+                //Creem un objecte word a partir de la linea llegida
+                Words word = new Words(linea);
+                //Guardem l'objecte en el array
+                w[contador] = word;
+                contador++;
+
+                //Imprimim el fitxer
+				System.out.println(linea);
 		}
         
 		//Cal tancar el fitxer
